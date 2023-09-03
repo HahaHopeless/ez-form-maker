9d91cd9dec9a3a3fb9e9378285ac15ed59e20ee3
Mohsin Kamal Akbar <mohsin.kamal.akbar@gmail.com>
2023-09-03 20:20:37 +0200

    update initial state for formData

--- src/context.tsx
+++ src/context.tsx
@@ -22,59 +22,7 @@ export const AppContext = createContext<AppContextProps>({} as AppContextProps);
 
 export const AppContextProvider = ({ children }: AppProviderProps) => {
   const [tab, setTab] = useState("result");
-  const [formData, setFormData] = useState<Array<{}>>([
-    {
-      control: "textfield",
-      id: "textfield",
-      label: "Text Field",
-      required: "true",
-    },
-    {
-      control: "textarea",
-      id: "textarea",
-      label: "Text Area",
-    },
-    {
-      control: "dropdown",
-      id: "dropdown",
-      label: "Dropdown",
-      options: [
-        {
-          value: "1",
-          label: "Option 1",
-        },
-        {
-          value: "2",
-          label: "Option 2",
-        },
-      ],
-    },
-    {
-      control: "radiogroup",
-      id: "radiogroup",
-      label: "Radio Group",
-      options: [
-        {
-          value: "3",
-          label: "Radio 1",
-        },
-        {
-          value: "4",
-          label: "Radio 2",
-        },
-      ],
-    },
-    {
-      control: "date",
-      id: "date",
-      label: "Date",
-    },
-    {
-      control: "button",
-      id: "button",
-      label: "Button",
-    },
-  ]);
+  const [formData, setFormData] = useState<Array<{}>>([]);
   const [formString, setFormString] = useState<string>("");
   return (
     <AppContext.Provider
